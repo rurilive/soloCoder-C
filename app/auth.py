b0564b8366c72909a4e3df8ca19c022dd86da222
ruri <m5@ruri.live>
2026-04-25 23:01:35 +0800

    5476

--- app/auth.py
+++ app/auth.py
@@ -2,7 +2,7 @@ from typing import Optional
 from fastapi import Request, HTTPException, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.orm import Session
-from app.database import get_db, User, INITIAL_TOKEN
+from app.database import get_db, User
 
 security = HTTPBearer(auto_error=False)
 
